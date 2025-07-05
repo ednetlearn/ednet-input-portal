a91e224d25065c20ebec3141389e73310e133514
ednetlearn <seshakalpana7@gmail.com>
2025-07-05 18:09:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      // If you want to exclude certain Node modules like Airtable f
+});
